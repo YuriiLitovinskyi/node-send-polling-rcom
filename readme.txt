226389097c188afa204b43d2213d38e22ccd6548
YuriiLitovinskyi <y.litovinskyi@gmail.com>
2020-04-19 17:04:32 +0300

    v.1

--- readme.txt
+++ readme.txt
@@ -3,3 +3,12 @@ npm init
 "dependencies": {
   "mongodb": "2.2.33"
 }
+
+  npm i prompt-confirm
+  npm install -g pkg
+Create .exe file for win x64:
+  pkg server.js
+Create .exe file for win x86:
+  pkg server.js --targets=latest-win-x86
+
+https://www.youtube.com/watch?v=lIpmtAs-wGk
--- server.js
+++ server.js
@@ -1,29 +1,56 @@
 const MongoClient = require('mongodb').MongoClient;
+const Confirm = require('prompt-confirm');
+const prompt = new Confirm('Do you want to continue?');
 
 const url = 'mongodb://localhost:27017/DBClientsPPK';
 
-(async () => {
-    const connectToMongo = await  MongoClient.connect(url, async (err, db) => {
+(() => {
+    MongoClient.connect(url, async (err, db) => {
         if(err){
-            console.log('No connection to Database! Please start MongoDB service on default port 27017!\n');                       
-            
+            console.log('No connection to Database! Please start MongoDB service on default port 27017!\n');                         
             console.log(err);
             await sleep(10000);
         } else {
-            console.log('Connected to database successfully!\n');
+            console.log('Connected to database successfully!\n');            
 
-           countAllOnlinePpk(db, async (ppkArray) => {
-                console.log("ppkArray", ppkArray);
+            getOnlinePpks(db, async (ppkArray) => {
+                //console.log("ppkArray", ppkArray);
 
                 if(ppkArray.length > 0){
+                    ppkArray.sort((a, b) => a.ppk_num - b.ppk_num);
 
+                    console.log(`Polling command will be sent to each device online with timeout of 60 seconds!`);
+                    console.log(`Total time for program execution: ${ppkArray.length} minutes\n`)
+
+                    prompt.ask((answer) => {                       
+                        if(answer){
+                            let counter = 0;
+                            ppkArray.forEach((ppk, index) => {
+                                setTimeout(async () => {
+                                    await sendPollingToPpk(db, ppk.ppk_num, () => {
+                                        console.log(`Polling sent to ppk number ${ppk.ppk_num}`);                               
+                                    });   
+                                    
+                                    counter++;
+                                    if(counter === ppkArray.length){
+                                        setTimeout(async () => {
+                                            console.log('All ppks received polling! Application will be closed!');
+                                            db.close();
+                                            await sleep(5000);
+                                        }, 5000);                                
+                                    };
+                                }, index * 1000);
+                            });
+                        } else {
+                            db.close();
+                            return null;
+                        }               
+                    });                  
                 } else {
                     console.log('No ppk online...')
                     db.close();
                     await sleep(10000);
                 }
-
-                db.close();
                 await sleep(10000);
             });
             
@@ -39,24 +66,72 @@ const sleep = (timeout) => {
     });
 };
 
-const countAllOnlinePpk = (db, callback) => {
+// // Collection 'ppkState'
+// const countAllOnlinePpk = (db, callback) => {
+//     setTimeout(() => {
+//         db.collection('ppkState', async (err, collection) => {
+//             if(err) {
+//                 console.log(err);
+//                 db.close();
+//                 await sleep(10000);
+//             };
+      
+//             // Get all ppk online at the moment from collection ppkState
+//             const onlinePpk = await collection.find({ markedAsOffline: false }).count();
+//             console.log(`Total online ppks at the moment: ${onlinePpk}`); 
+
+//             const onlinePpkObj = await collection.find({ markedAsOffline: false }).toArray((err, ppkArray) => {
+                
+//                 callback(ppkArray);
+
+//             });                    
+//         });        
+//     }, 1000);    
+// };
+
+
+// Collection 'coll_ping'
+const getOnlinePpks = (db, callback) => {
     setTimeout(() => {
-        db.collection('ppkState', async (err, collection) => {
+        db.collection('coll_ping', async (err, collection) => {
             if(err) {
                 console.log(err);
                 db.close();
                 await sleep(10000);
             };
       
-            // Get all ppk online at the moment
-            const onlinePpk = await collection.find({ markedAsOffline: false }).count();
-            console.log(`Total online ppks at the moment: ${onlinePpk}`); 
+            // Get all ppk online at the moment   4 * 60 = 4 minutes
+            const onlinePpk = await collection.find({ time: {$lt: Date.now() - 172800 }}).count();
+            console.log(`Total online ppks at the moment: ${onlinePpk}\n`); 
 
-            const onlinePpkObj = await collection.find({ markedAsOffline: false }).toArray((err, ppkArray) => {
-                
+            // Get array of online ppks
+            await collection.find({ time: {$lt: Date.now() - 172800 }}).toArray((err, ppkArray) => {
+                if(err) {
+                    console.log(err);
+                    db.close();                    
+                };                
                 callback(ppkArray);
-
-            });                    
+            });                          
         });        
     }, 1000);    
+};
+
+// Insert query polling in Collection 'coll_ping'
+const sendPollingToPpk = (db, ppk_num, callback) => {
+    db.collection('coll_ping', async (err, collection) => {
+        if(err) {
+            console.log(err);
+            db.close();
+            await sleep(10000);
+        };
+
+        await collection.updateOne({ ppk_num : ppk_num },
+            { $set: { query : 48 } }, (err, result) => {
+                if(err){
+                    console.log(err);
+                };         
+            console.log(`${result}\n`);          
+            });  
+        callback();
+    });
 };
